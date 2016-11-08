539a82a10bda51967ca7f4381215ee34c650004e
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-07 21:30:47 -0500

    Added file reading function

--- src/ebirdlistgenerator/Data.java
+++ src/ebirdlistgenerator/Data.java
@@ -1,18 +1,36 @@
 package ebirdlistgenerator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Brandon
  */
 public class Data 
 {
+    ArrayList <Checklist> list;
     public Data ()
     {
-        
+        list = new ArrayList<>();
     }
     
     public void loadData (String filename)
     {
-        
+        try
+        {
+            File f = new File(filename);
+            Scanner file = new Scanner(f);
+            while (file.hasNextLine())
+            {
+                //parse
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Could not find file");
+        }
     }
 }
