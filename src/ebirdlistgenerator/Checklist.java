9fb1d0fa92b0ff5195a8240f5cb9425996d6898b
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-07 21:27:03 -0500

    Added Checklist class

---
+++ src/ebirdlistgenerator/Checklist.java
@@ -0,0 +1,17 @@
+package ebirdlistgenerator;
+
+import java.util.ArrayList;
+
+public class Checklist 
+{
+    private String subID, state_prov, county, location, latitude, longitude,
+            date, time, duration, distance, area, numObservers, comments;
+    ArrayList <Bird> bird;
+    
+    public Checklist ()
+    {
+        bird = new ArrayList<>();
+    }
+    
+    
+}
