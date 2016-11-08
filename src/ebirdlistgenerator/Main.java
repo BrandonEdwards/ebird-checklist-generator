ac998d5b8bcae864d984d6832f58f6660fbf0668
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-07 21:18:22 -0500

    Added some class files

---
+++ src/ebirdlistgenerator/Main.java
@@ -0,0 +1,10 @@
+package ebirdlistgenerator;
+
+public class Main 
+{
+    public static void main (String args[])
+    {
+        Data data = new Data(args[1]);
+        loadData(args[1]);
+    }
+}
