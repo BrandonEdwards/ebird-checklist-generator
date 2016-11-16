dd5f2fea79cc1b369647c2fcaff59b5a177c4314
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-16 11:57:51 -0500

    Added bird class constructor

--- src/ebirdlistgenerator/Bird.java
+++ src/ebirdlistgenerator/Bird.java
@@ -6,6 +6,20 @@ public class Bird
     
     public Bird ()
     {
-        
+        this.commonName = "";
+        this.scientificName = "";
+        this.count = "";
+        this.breedingCode = "";
+        this.comments = "";
+    }
+    
+    public Bird (String commonName, String scientificName, String count,
+            String breedingCode, String comments)
+    {
+        this.commonName = commonName;
+        this.scientificName = scientificName;
+        this.count = count;
+        this.breedingCode = breedingCode;
+        this.comments = comments;
     }
 }
