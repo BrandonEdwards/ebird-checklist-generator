63a9437e47614a373b9b99143b16cb64e7627dd6
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-16 12:12:30 -0500

    Added setter methods

--- src/ebirdlistgenerator/Checklist.java
+++ src/ebirdlistgenerator/Checklist.java
@@ -24,5 +24,51 @@ public class Checklist
         return this.subID;
     }
     
+    public void setStateProv (String state_prov)
+    {
+        this.state_prov = state_prov;
+    }
+    
+    public void setCounty (String county)
+    {
+        this.county = county;
+    }
+    
+    public void setLocation (String location)
+    {
+        this.location = location;
+    }
+    
+    public void setLatLong (String latitude, String longitude)
+    {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+    public void setDateTime (String date, String time, String duration)
+    {
+        this.date = date;
+        this.time = time;
+        this.duration = duration;
+    }
+    
+    public void setDistArea (String distance, String area)
+    {
+        this.distance = distance;
+        this.area = area;
+    }
+    
+    public void setNumObservers (String numObservers)
+    {
+        this.numObservers = numObservers;
+    }
+    
+    public void setComments (String comments)
+    {
+        this.comments = comments;
+    }
+    
+    
+    
     
 }
