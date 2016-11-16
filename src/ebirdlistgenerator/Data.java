edf1a03646bd13b0c7fa44ef0c9238e98003d2bb
Brandon Edwards <brandon.edwards144@gmail.com>
2016-11-16 11:49:49 -0500

    Added hashmap capabilities for subID

--- src/ebirdlistgenerator/Data.java
+++ src/ebirdlistgenerator/Data.java
@@ -3,6 +3,7 @@ package ebirdlistgenerator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -12,9 +13,11 @@ import java.util.Scanner;
 public class Data 
 {
     ArrayList <Checklist> list;
+    HashMap <String, Integer> subIDMap;
     public Data ()
     {
         list = new ArrayList<>();
+        subIDMap = new HashMap<>();
     }
     
     public void loadData (String filename)
@@ -39,28 +42,11 @@ public class Data
         //Regex from http://stackoverflow.com/questions/15738918/splitting-a-csv-file-with-quotes-as-text-delimiter-using-string-split
         String[] elements = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
         
-        if (checklistExists(elements[0]))
+        if (subIDMap.get(elements[0]) == null)
         {
             
         }
 
     }
     
-    private boolean checklistExists (String subID)
-    {
-        if (list.isEmpty())
-        {
-            return false;
-        }
-        
-        for (int i = 0; i < list.size(); i++)
-        {
-            if (list.get(i).getSubID().equals(subID))
-            {
-                return true;
-            }
-        }
-        
-        return false;
-    }
 }
